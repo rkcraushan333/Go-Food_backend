2bacab937ef0ac5daeacb075dfcf640b50dc2dd5
rkcraushan333 <raushank.ug20.ece@nitp.ac.in>
2023-10-30 16:31:58 +0530

    updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const mongoDB = require('./db');
 mongoDB();
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "https://go-food-frontend-gamma.vercel.app/");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
