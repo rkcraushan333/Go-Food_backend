7a697cff8be669a73254a04f5cab5a048a72f079
rkcraushan333 <raushank.ug20.ece@nitp.ac.in>
2023-10-30 16:04:21 +0530

    commited backend part

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const mongoDB = require('./db');
+mongoDB();
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    next();
+})
+app.use(express.json())
+app.use('/api', require("./Routes/CreateUser"))
+app.use('/api', require("./Routes/DisplayData"))
+app.use('/api', require("./Routes/OrderData"))
+
+app.get('/', (req, res) => {
+    res.send('Ha bhai 5000 chal rha hai')
+})
+app.listen(5000, () => {
+    console.log(`Server is running on port 5000`)
+})
